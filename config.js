bc803f3497db12d38e814757a8fc1c2057b2f9d6
anovsiradj <anov.siradj22@gmail.com>
2017-11-02 17:15:20 +0700

    update UA string to chrome-v62

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const myConfigData = {
 	do_spoof_ua: true,
 	do_inject_shim: true,
 
-	ua_string: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36',
+	ua_string: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
 	ua_vendor: 'Google, Inc.',
 	ua_platform: 'Windows',
 
